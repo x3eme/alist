9b828ce0d78908c9f23207736cb65fe575d70070
shahin <shahin@gmail.com>
2011-11-14 15:51:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alist::Application.config.secret_token = '7f4cfcc4c848b43b4f29bd531c45400c79bdafd9d11fe0c1583b32fea6bdb628d326f92102bd24302dc7117dd284592386c923934d04a9605c327992aa3740b8'
